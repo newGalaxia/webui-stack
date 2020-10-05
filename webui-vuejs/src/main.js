d61dd429171561fd86d2dea33e2e7c5bad83aadf
s.fournage@gmail.com <s.fournage@gmail.com>
2020-10-05 11:43:30 +0200

    feat: add router plugin

--- webui-vuejs/src/main.js
+++ webui-vuejs/src/main.js
@@ -2,22 +2,36 @@ import "@babel/polyfill";
 import Vue from "vue";
 import App from "./App.vue";
 import { makeServer } from "./server";
-// import VueRouter from 'vue-router'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import VueRouter from "vue-router";
+import Dashboard from "./components/Dashboard.vue"
+import Wlan from "./components/Wlan.vue"
 
-// Vue.use(VueRouter)
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
+Vue.use(VueRouter);
 // Install BootstrapVue
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
 if (process.env.NODE_ENV === "development") {
   makeServer();
 }
 Vue.config.productionTip = false;
+const routes = [
+  { path: '/', component: Dashboard },
+  { path: '/wlan', component: Wlan },
+  { path: '/dashboard', component: Dashboard },
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 
 new Vue({
   render: (h) => h(App),
+  router
 }).$mount("#app");
