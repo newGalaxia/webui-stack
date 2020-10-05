ebea41e50b06cf15fc28f9a499b261c7dd10ecf4
s.fournage@gmail.com <s.fournage@gmail.com>
2020-10-05 11:43:30 +0200

    feat: add router plugin

--- webui-vuejs/src/App.vue
+++ webui-vuejs/src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
     <Navbar />
-    <Wlan />
+    <router-view />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
-import Wlan from "./components/Wlan.vue";
+
 export default {
   name: "App",
   components: {
     Navbar,
-
-    Wlan,
   },
 };
 </script>
--- webui-vuejs/src/components/Navbar.vue
+++ webui-vuejs/src/components/Navbar.vue
@@ -7,7 +7,10 @@
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
         <b-nav-item-dropdown text="Etat" right>
-          <b-dropdown-item href="#">Général</b-dropdown-item>
+          <b-dropdown-item href="#">    <router-link to="/dashboard">Général</router-link>
+</b-dropdown-item>
+<b-dropdown-item href="#">    <router-link to="/wlan">Wlan</router-link>
+</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
 
